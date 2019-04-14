d94f93de1b5a1416b71226ae04899ed7000e1c6b
iotoiletgithub <49594944+iotoiletgithub@users.noreply.github.com>
2019-04-14 09:01:19 +0800

    Add files via upload

---
+++ fadel.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "mang";
+$password = "P!$\$W)RD";
+$dbname = "mindcontrol";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "DELETE FROM existence WHERE incognito is NOT NULL";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
